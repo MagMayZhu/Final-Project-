7acb6eb6103a73bd4bab66e850cb23ad7d10a906
Madison Anspach <madison.anspach22@kzoo.edu>
2025-05-14 12:58:52 -0400

    Add basic input validation in controller

---
+++ AppController.java
@@ -0,0 +1,10 @@
+public class AppController {
+    public String getWelcomeMessage() {
+        return "Welcome to the app!";
+    }
+
+    public String processInput(String input) {
+        // Example logic: echo the input
+        return "You entered: " + input;
+    }
+}
